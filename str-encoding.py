258814cc9827867e19e868526c3f883c2f9c5a1a
Olivier HENRY <75161856+Flying-Tree@users.noreply.github.com>
2021-11-19 20:57:52 +0100

    Add files via upload

---
+++ str-encoding.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 20:14:06 2021
+
+@author: Olivier Henry
+"""
+
+# Scripts to encode and decode strings
+
+from math import *
+
+# =============================================================================
+# HUFFMAN CODING
+# =============================================================================
+
+def tree_2_code(tree,bin_code):
+    # Auxiliar function for huffman_encode
+    # Recursive function transforming a tree into an association of letters and bin sequences
+    k=[]
+    for i,x in enumerate(tree):
+        if type(x)==str:
+            k.append((x,bin_code+str(i)))
+        else:
+            k+=tree_2_code(x,bin_code+str(i))
+    return k
+
+def huffman_encode(message):
+    tree=[]
+    
+    # Count
+    for letter in message:
+        if not letter in [x[0] for x in tree]:
+            tree.append([letter,message.count(letter)])
+    
+    # Make tree
+    while len(tree)>1:
+        tree.sort(key=lambda x:x[1])
+        tree.insert(0,[[tree[0][0],tree[1][0]],tree[0][1]+tree[1][1]])
+        tree.pop(1)
+        tree.pop(1)
+    tree=tree[0][0]
+    
+    # Make code
+    code_list=tree_2_code(tree,'')
+    code_dic={x[0]:x[1] for x in code_list}
+    
+    # Encode
+    coded=""
+    for letter in message:
+        coded+=code_dic[letter]
+    
+    print(len(coded)/len(message))
+    print(log(len(code_list),2))
+    
+    return coded,tree
+
+def huffman_decode(coded,tree):
+    message=""
+    tree_rel=tree
+    for x in coded:
+        if type(tree_rel[int(x)])==str:
+            message+=tree_rel[int(x)]
+            tree_rel=tree
+        else:
+            tree_rel=tree_rel[int(x)]
+  
+    return message
